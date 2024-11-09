40372aec1c1035e0c488dff5d6f32a778c48459e
panderonak <pandeyronak139@gmail.com>
2024-11-09 13:50:59 +0530

    Created reusable components: Button, Container, LogoutButton, Input, and Logo.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import Logo from "./Logo/Logo";
+import LogoutButton from "./Header/LogoutButton";
+import Button from "./Button/Button";
+import Input from "./Input/input";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutButton, Button, Input };
