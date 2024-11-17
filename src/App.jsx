5b780b2b82457aee5556e67e8b6281987392453d
panderonak <pandeyronak139@gmail.com>
2024-11-17 19:34:09 +0530

    fix: resolve login, post display, and other issues across multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,19 @@ function App() {
         <Footer />
       </div>
     </div>
-  ) : null;
+  ) : (
+    <div className="w-full block">
+      <div className="min-h-screen bg-[#6B5CA5] text-center font-mono text-3xl">
+        <div className="flex h-screen w-full items-center justify-center gap-x-2">
+          <div className="border-r[#00A896]  flex h-5 w-60 items-center justify-center rounded-full p-5 animate-bounce">
+            <div className="border-r[#00A896] bg-black h-20 w-20 animate-spin rounded-full border-e-8 border-l-8 border-b-[#6B5CA5] shadow-2xl flex justify-center items-center">
+              <div className="border-r[#00A896] bg-green-950 h-10 w-20 animate-spin rounded-full border-e-8 border-l-8 border-b-[#6B5CA5] shadow-2xl"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
