665e2b501cb7db0bcf2a1303d21efa251e3d3a74
panderonak <pandeyronak139@gmail.com>
2024-12-01 17:38:25 +0530

    feat: improve blog UI with enhanced layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 function Header() {
   const authStatus = useSelector((state) => state.auth.status);
+  console.log(`Header ${authStatus}`);
   const navigate = useNavigate();
 
   const navItems = [
