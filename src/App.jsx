edc4a52b8edb832370561b6422e18f3a3be879b0
panderonak <pandeyronak139@gmail.com>
2024-12-22 15:59:09 +0530

    feat: add LogOutModal component in App.jsx and implement loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth-service";
 import { login, logout } from "./features/authSlice";
-import { Header, Footer } from "./components";
+import { Header, Footer, LogOutModal } from "./components";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
+  const popUpStatus = useSelector((state) => state.popUp.isVisible);
 
   useEffect(() => {
     authService
@@ -26,6 +28,7 @@ function App() {
     <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
       <div className="w-full block">
         <Header />
+        {popUpStatus && <LogOutModal />}
         <main className="bg-[white] p-10">
           <Outlet />
         </main>
@@ -33,14 +36,11 @@ function App() {
       </div>
     </div>
   ) : (
-    <div className="w-full block">
-      <div className="min-h-screen bg-[#6B5CA5] text-center font-mono text-3xl">
-        <div className="flex h-screen w-full items-center justify-center gap-x-2">
-          <div className="border-r[#00A896]  flex h-5 w-60 items-center justify-center rounded-full p-5 animate-bounce">
-            <div className="border-r[#00A896] bg-black h-20 w-20 animate-spin rounded-full border-e-8 border-l-8 border-b-[#6B5CA5] shadow-2xl flex justify-center items-center">
-              <div className="border-r[#00A896] bg-green-950 h-10 w-20 animate-spin rounded-full border-e-8 border-l-8 border-b-[#6B5CA5] shadow-2xl"></div>
-            </div>
-          </div>
+    <div className="block w-full">
+      <div className="h-screen bg-white flex justify-center items-center">
+        <div className="relative flex justify-center items-center">
+          <div className="absolute h-20 w-20 rounded-full shadow-[inset_0_0_10px_2px_rgba(211,211,211,0.6)]"></div>
+          <div className="absolute h-20 w-20 animate-spin rounded-full shadow-[inset_0_2px_0_#000]"></div>
         </div>
       </div>
     </div>
