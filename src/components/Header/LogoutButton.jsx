7770c8a5ddd42e2edeafceff974454799e15f4cf
panderonak <pandeyronak139@gmail.com>
2024-12-22 15:52:27 +0530

    feat: add logout confirmation popup

--- src/components/Header/LogoutButton.jsx
+++ src/components/Header/LogoutButton.jsx
@@ -1,27 +1,21 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
-import authService from "../../appwrite/auth-service";
-import { logout } from "../../features/authSlice";
-import { useNavigate } from "react-router-dom";
+import { showPopUp } from "../../features/logoutPopup";
 
 function LogoutButton() {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
   const logoutHandler = () => {
-    console.log("Logout called");
-    authService.logout().then(() => {
-      dispatch(logout());
-    });
-    // dispatch(clearAllPosts());
-
-    navigate("/");
+    dispatch(showPopUp());
   };
   return (
-    <button
-      className="inline-bock px-6 py-2 duration-200 hover:bg-[#890620] rounded-full"
-      onClick={logoutHandler}
-    >
-      Logout
-    </button>
+    <>
+      <button
+        className="inline-bock px-6 py-2 duration-200 hover:bg-[#890620] rounded-full"
+        onClick={logoutHandler}
+      >
+        Logout
+      </button>
+    </>
   );
 }
 
