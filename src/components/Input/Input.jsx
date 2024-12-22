fdf8f0cc230f797b1a3af4bf1b531b12419082ed
panderonak <pandeyronak139@gmail.com>
2024-12-22 16:05:27 +0530

    style: enhance UI for better user experience

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -6,14 +6,17 @@ const Input = React.forwardRef(function Input(
 ) {
   const id = useId();
   return (
-    <div className="">
+    <div className="mb-3">
       {label && (
-        <label className="inline-block mb-1 pl-1" htmlFor={id}>
+        <label
+          className="inline-block mb-1 text-base font-light pl-1"
+          htmlFor={id}
+        >
           {label}
         </label>
       )}
       <input
-        className={`px-5 py-3 rounded-3xl bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        className={`px-5 py-3 rounded-xl bg-white text-black outline-none focus:bg-gray-50 focus:border-[black] duration-200 border border-[#CEDADA] w-full ${className}`}
         type={type}
         ref={ref}
         {...props}
